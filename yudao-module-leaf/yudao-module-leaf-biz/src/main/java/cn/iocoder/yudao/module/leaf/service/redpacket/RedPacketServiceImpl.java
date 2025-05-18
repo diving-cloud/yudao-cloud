ee9389806a677087e955b349bb01dec93a3cc57b
hezongkui <zongkuihe@gmail.com>
2025-05-18 13:10:34 +0800

    移除冗余导入

--- yudao-module-leaf/yudao-module-leaf-biz/src/main/java/cn/iocoder/yudao/module/leaf/service/redpacket/RedPacketServiceImpl.java
+++ yudao-module-leaf/yudao-module-leaf-biz/src/main/java/cn/iocoder/yudao/module/leaf/service/redpacket/RedPacketServiceImpl.java
@@ -26,7 +26,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
